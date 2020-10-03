240c6b95287d9628bcdf265d41009bfa7cd580f4
Pragya Kant Jha <pkjakv123@gmail.com>
2020-10-03 12:30:03 +0530

    ..

--- while.sh
+++ while.sh
@@ -8,10 +8,9 @@ then
     while [ $n -lt 20 ] # or (( $n -lt 20 )) both can be used
     do 
         echo "$((n+1))"
-        (( n++ ))    
+        (( n++ ))
+        sleep=1 # will print the value after 1 sec apart.    
     done
 else
     echo "you should have entered less than 20 to see the magic."
 fi
-
-
