0abff2344f6de4c8bf113077bcf8490c96cfd7b0
Pragya Kant Jha <pkjakv123@gmail.com>
2020-10-03 12:23:35 +0530

    while loop explained

--- while.sh
+++ while.sh
@@ -1,1 +1,17 @@
 #! /bin/bash
+
+echo -e "Enter the no: \c"
+read n
+
+if [ $n -lt 20 ]
+then
+    while [ $n -lt 20 ] # or (( $n -lt 20 )) both can be used
+    do 
+        echo "$((n+1))"
+        (( n++ ))    
+    done
+else
+    echo "you should have entered less than 20 to see the magic."
+fi
+
+
