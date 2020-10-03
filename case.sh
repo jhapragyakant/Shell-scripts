ed8079da6d0041db0b1ce2e6d53b14c97f792f2a
Pragya Kant Jha <pkjakv123@gmail.com>
2020-10-03 11:14:48 +0530

    modified

--- case.sh
+++ case.sh
@@ -1,2 +1,37 @@
 #! /bin/bash
 
+echo -e "Enter a charecter: \c"
+read value
+
+<<'COMMENTS'
+case $vehcile in 
+    "car" )
+        echo "Rent of $vehcile is Rs.100";;
+    "Motorbike" )
+        echo "Rent of $vehcile is Rs.50";;
+    "Rikshaw" )
+        echo "Rent of $vehcile is Rs.20";;
+    "Truck" )
+        echo "Rent of $vehcile is Rs.2000";;
+    "Bicycle" )
+        echo "Rent of $vehcile is Rs.10";;
+    * )
+        echo "Unkown Vechile";;
+esac
+COMMENTS
+
+case $value in
+    [a-z] )
+        echo "Entered character is fron a-z";;
+    [A-Z] )
+        echo "Entered character is fron A-Z";;
+    [0-9] )
+        echo "Entered character is fron 0-9";;
+    ? )
+        echo "Entered character is a special character";;
+    * )
+        echo "Not a valid character or a string";;
+esac
+
+# for multi-line comment just write anything whithin <<'' and at last write the same thing 
+# eg-> <<'comment' ...................... comment
